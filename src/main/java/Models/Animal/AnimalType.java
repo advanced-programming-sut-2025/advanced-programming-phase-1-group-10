48ec5dc4210453a9f45400d143c43362c758db10
ilia.es <ilia40471@gmail.com>
2025-05-09 06:37:33 -0700

    Barn&Coop updated.

--- src/main/java/Models/Animal/AnimalType.java
+++ src/main/java/Models/Animal/AnimalType.java
@@ -1,32 +1,25 @@
 package Models.Animal;
 
+import Models.Place.Maintenance;
+
 public enum AnimalType {
-    CHICKEN("chicken","Coop", 800),
-    DUCK("duck","Coop", 1200),
-    RABBIT("rabbit","Coop", 8000),
-    DINOSAUR("dinosaur","Coop", 14000),
-    COW("cow","Barn", 1500),
-    GOAT("goat","Barn", 4000),
-    SHEEP("sheep","Barn", 8000),
-    PIG("pig","Barn", 16000);
+    CHICKEN("chicken",Maintenance.Coop, 800),
+    DUCK("duck",Maintenance.Coop, 1200),
+    RABBIT("rabbit",Maintenance.Coop, 8000),
+    DINOSAUR("dinosaur",Maintenance.Coop, 14000),
+    COW("cow",Maintenance.Barn, 1500),
+    GOAT("goat",Maintenance.Barn, 4000),
+    SHEEP("sheep",Maintenance.Barn, 8000),
+    PIG("pig",Maintenance.Barn, 16000);
 
 
     private final String type;
-    private final String enclosures ;
+    private final Maintenance enclosures ;
     private final int price ;
 
-    AnimalType(String type, String enclosures, int price) {
+    AnimalType(String type, Maintenance enclosures, int price) {
         this.type = type;
         this.enclosures = enclosures;
         this.price = price;
-    }
-
-    public static AnimalType fromString(String type){
-        for(AnimalType animalType : AnimalType.values()){
-            if(animalType.type.equalsIgnoreCase(type)){
-                return animalType;
-            }
-        }
-        return null;
     }
 }
