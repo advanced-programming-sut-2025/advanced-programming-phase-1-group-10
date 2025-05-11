4eb301dd1ac560e471757712d77f131fef3b3c91
reza <reza.khoini@gmail.com>
2025-05-11 09:55:01 +0330

    Crafting Done

---
+++ src/main/java/Models/Bar/Bar.java
@@ -0,0 +1,37 @@
+package Models.Bar;
+
+import Models.Item;
+
+public class Bar implements Item {
+
+    private BarType barType;
+    private int numberOfBar;
+
+    public Bar(BarType barType, int numberOfBar) {
+        this.barType = barType;
+        this.numberOfBar = numberOfBar;
+    }
+
+    @Override
+    public String getName(){
+        return barType.getName();
+    }
+
+    @Override
+    public String getSymbol() {
+        return "b";
+    }
+
+    @Override
+    public int getNumber() {
+        return numberOfBar;
+    }
+
+    @Override
+    public void setNumber(int number) {
+        numberOfBar = number;
+    }
+
+
+
+}
---
+++ src/main/java/Models/Bar/BarType.java
@@ -0,0 +1,22 @@
+package Models.Bar;
+
+public enum BarType {
+
+    COPPER_BAR ("Copper Bar"),
+    IRON_BAR ("Iron Bar"),
+    GOLD_BAR  ("Gold Bar"),
+    IRIDIUM_BAR("Iridium Bar"),
+    RADIOACTIVE_BAR("Radioactive Bar")
+        ;
+
+    private final String name;
+
+    BarType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
