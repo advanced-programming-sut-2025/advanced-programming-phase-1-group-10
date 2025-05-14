4d44dbbeafb020fd819dc30e61d1a5ffde103f24
AmirEsfandiari <esfandiari.hq@gmail.com>
2025-05-14 06:59:37 +0330

    Map has background color!

--- src/main/java/Models/Tile.java
+++ src/main/java/Models/Tile.java
@@ -93,22 +93,90 @@ public class Tile {
     }
 
     public String getTile() {
+        String ch;
         if (this.person != null) {
-            return this.person.getSymbol();
-        } else if(this.item != null) {
-            return (item.getSymbol());
+            ch = this.person.getSymbol();
+        } else if (this.item != null) {
+            ch = (item.getSymbol());
         } else if (this.getAnimal() != null) {
-            return "*";
+            ch = "*";
         } else if (this.getTileType() == TileType.Wall) {
-            return "X";
-        } else if(this.getisPlow()) {
-            return "0";
-        } else if (this.place != null) {
-            return place.getSymbol();
+            ch = "X";
+        } else if (this.getisPlow()) {
+            ch = "0";
         } else {
-            return " ";
+            ch = " ";
         }
+        return getColoredText(ch);
+    }
+
+    public static final String RESET = "\u001B[0m";
+    public static final String RED_BACKGROUND = "\u001B[41m";
+    public static final String GREEN_BACKGROUND = "\u001B[42m";
+    public static final String YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String BLUE_BACKGROUND = "\u001B[44m";
+    public static final String PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String WHITE_BACKGROUND = "\u001B[47m";
+    public static final String BLACK_BACKGROUND = "\u001B[40m";
+    public static final String BRIGHT_YELLOW_TEXT = "\u001B[93m"; // brighter than normal yellow
+
+
+    public static final String BLACK_TEXT = "\u001B[30m";
+    public static final String WHITE_TEXT = "\u001B[37m";
+
+    public static final String BOLD = "\u001B[1m";
+
+    public String getColoredText(String ch) {
+        if (place == null) return WHITE_BACKGROUND + BLACK_TEXT + ch + RESET;
+
+        String bgColor;
+        String textColor = BLACK_TEXT;
+        String style = "";
+
+        switch (place.getSymbol()) {
+            case "H":
+                bgColor = RED_BACKGROUND;
+                break;
+            case "Q":
+                bgColor = BLACK_BACKGROUND;
+                textColor = WHITE_TEXT;
+                break;
+            case "C":
+                bgColor = PURPLE_BACKGROUND;
+                break;
+            case " ":
+                bgColor = WHITE_BACKGROUND;
+                break;
+            case "W":
+                bgColor = BLUE_BACKGROUND;
+                break;
+            case "G":
+                bgColor = GREEN_BACKGROUND;
+                break;
+            case "B":
+                bgColor = YELLOW_BACKGROUND;
+                break;
+            case "~":
+                bgColor = PURPLE_BACKGROUND;
+                break;
+            case "-":
+                bgColor = YELLOW_BACKGROUND;
+                break;
+            default:
+                bgColor = BLACK_BACKGROUND;
+                textColor = WHITE_TEXT;
+        }
+
+        // Special styling for player "P"
+        if (ch.equals("P")) {
+            style = BOLD;
+            textColor = BRIGHT_YELLOW_TEXT;
+        }
+
+        return bgColor + textColor + style + ch + RESET;
     }
 
 }
+
+
 
