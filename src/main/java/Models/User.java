a729b579dec0e39fdee7e623630c0577b4db762b
ilia.es <ilia40471@gmail.com>
2025-05-17 08:52:28 -0700

    emtyaizy

--- src/main/java/Models/User.java
+++ src/main/java/Models/User.java
@@ -2,13 +2,20 @@ package Models;
 
 import Models.PlayerStuff.Gender;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
 public class User {
     private String nickname;
     private String password;
+    private String hashPassword;
     private String username;
     private Gender gender;
     private String email;
@@ -19,11 +26,40 @@ public class User {
     public int games ;
 
     public User(String nickname, String password, String username, Gender gender) {
-        this.nickname = nickname;
         this.password = password;
+        this.hashPassword =generateSaltAndHashPassword(password);
+        this.nickname = nickname;
         this.username = username;
         this.gender = gender;
         this.pickQuestion = new HashMap<>();
+    }
+
+    private String generateSaltAndHashPassword(String password) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        String saltString = Base64.getEncoder().encodeToString(salt);
+
+        String hashedPassword = hashPassword(password, saltString);
+        return saltString + ":" + hashedPassword;
+    }
+
+    private String hashPassword(String password, String salt) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            String saltedPassword = password + salt;
+            byte[] hash = digest.digest(saltedPassword.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean authenticate(String password) {
+        String salt = this.hashPassword.split(":")[0];
+        String hashedPassword = hashPassword(password, salt);
+        return this.hashPassword.split(":")[1].equals(hashedPassword);
     }
 
     public boolean isStayLoggedIn() {
