ac6ce17ff6e79e9a9e155bb54002f35796e9bbbe
ilia.es <ilia40471@gmail.com>
2025-08-02 06:04:22 -0700

    cropTypeNormal assets added.

--- core/src/main/java/Models/Tools/SeedPlanter.java
+++ core/src/main/java/Models/Tools/SeedPlanter.java
@@ -22,9 +22,8 @@ public class SeedPlanter extends Tool {
 
     @Override
     public Sprite show() {
-        // این متد باید Sprite مربوط به SeedPlanter را برگرداند.
-        // toolAsset.show(this); // فرض می‌شود این متد وجود دارد
-        return null; // فعلا null برگردانده می‌شود
+
+        return null;
     }
 
 //    @Override
@@ -34,18 +33,14 @@ public class SeedPlanter extends Tool {
 //
 //        if (selectedItem instanceof Seed) {
 //            Seed seed = (Seed) selectedItem;
-//            // بررسی شرایط کاشت: کاشی باید شخم زده شده باشد و محصولی در آن نباشد.
 //            if (tile.isPlowed() && tile.getCrop() == null) {
-//                // محاسبه و کسر انرژی
 //                int energyCost = (int) ((getEnergyUsage() - getQuality().getValue()) * App.getInstance().getCurrentGame().getWeather().getToolEnergyModifer());
 //                if (player.getEnergy().getEnergyAmount() >= energyCost) {
 //                    player.getEnergy().setEnergyAmount(player.getEnergy().getEnergyAmount() - energyCost);
 //
-//                    // ایجاد Crop جدید و قرار دادن آن روی کاشی
 //                    Crop newCrop = new Crop(seed.getCropType());
 //                    tile.setCrop(newCrop);
 //
-//                    // کاهش تعداد بذر در کوله‌پشتی
 //                    if (seed.getNumber() > 1) {
 //                        seed.setNumber(seed.getNumber() - 1);
 //                    } else {
